35499dbc1226336aa2a12d0070864fad2fe20837
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2019-07-26 07:38:01 -0300

    Persistindo autenticação

---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,27 @@
+import { all, takeLatest, call, put } from "redux-saga/effects";
+
+// import history from "~/services/history";
+// import api from "~/services/api";
+// import { signInSuccess } from "./actions";
+
+// export function* signIn({ payload }) {
+//   const { email, password } = payload;
+
+//   const response = yield call(api.post, "sessions", {
+//     email,
+//     password
+//   });
+
+//   const { token, user } = response.data;
+
+//   if (!user.provider) {
+//     console.tron.error("Usuário não pe prestador");
+//     return;
+//   }
+
+//   yield put(signInSuccess(token, user));
+
+//   history.push("/dashboard");
+// }
+
+export default all([]);
